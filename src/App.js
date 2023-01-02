d21c6b716a39e3eb992c2348dc234e8f060e7d8d
olabee100 <olabayoantonio@gmail.com>
2023-01-03 01:25:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+// import { render } from '@testing-library/react';
+import React from 'react';
+import {HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Contacts from './Components/Contacts/Contacts';
+import AddContact from './Components/Contacts/AddContact';
+import EditContact from './Components/Contacts/EditContact';
+import Header from './Components/Layout/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Provider } from './context';
+import About  from './Components/pages/About';
+import NotFound  from './Components/pages/NotFound';
+import Test from './Components/test/Test';
+
 
 function App() {
+   
   return (
+    <Provider>
+      <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header branding="Contacts Manager1" />
+    <div className="container">
+    <Switch>
+      <Route exact path= "/" component = {Contacts}/>
+      <Route exact path= "/contact/add" component = {AddContact}/>
+      <Route exact path= "/contact/edit/:id" component = {EditContact}/>
+      <Route exact path= "/about" component = {About}/>
+      <Route exact path= "/test" component = {Test}/>
+      <Route component = {NotFound} />
+    </Switch>
     </div>
-  );
-}
+  </div>
+  </Router>
+  </Provider>
+   );
+  }
 
+  
 export default App;
